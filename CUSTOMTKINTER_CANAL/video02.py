4792f4072385f714af8c4cab0635a723dd0fdd7a
iSebas <jusecofe@gmail.com>
2025-08-24 18:35:18 -0500

    Se montan los recursos del segundo video donde damos los primeros pasos en customtkinter

--- CUSTOMTKINTER_CANAL/video02.py
+++ CUSTOMTKINTER_CANAL/video02.py
@@ -1,41 +1,34 @@
-import customtkinter as ctk
+import customtkinter
+
 
 def call_back_btn():
     print("Presionaste el boton")
 
-def call_back_btn1():
-    print("Presionaste el boton")
 
-app = ctk.CTk()
+app = customtkinter.CTk()
 
-ctk.set_appearance_mode("system")
-ctk.set_default_color_theme("dark-blue") 
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 
-app.title("iSebas APP")
 app.geometry("600x400")
+app.title("iSebas APP")
 
-button = ctk.CTkButton(app, text="Press Me", command=call_back_btn)
-button.grid( row=0, column =0,  padx=20,  pady=20,sticky="ew")
+button = customtkinter.CTkButton(app, text="Press Me", command=call_back_btn)
+button.grid( row=0, column=0,  padx= 10, pady=(40,0) )
 
-button1 = ctk.CTkButton(app, text="Press Me", command=call_back_btn1)
-button1.grid( row=0, column =1, padx=20,  pady=20)
+button2= customtkinter.CTkButton(app, text="Press Me", command=call_back_btn)
+button2.grid( row=0, column=1, padx= 10, pady=(40,0) )
 
-label1 = ctk.CTkLabel(app, text="TEXTO LBL", fg_color="transparent")
-label1.grid(row=0, column =2, padx=20,  pady=20)
+button3 = customtkinter.CTkButton(app, text="Press Me", command=call_back_btn)
+button3.grid( row=0, column=2, padx= 10, pady=(40,0) )
 
-label2 = ctk.CTkLabel(app, text="TEXTO LBL", fg_color="transparent")
-label2.grid(row=1, column =0, padx=20,  pady=20)
+button4= customtkinter.CTkButton(app, text="Press Me", command=call_back_btn)
+button4.grid( row=1, column=0,padx= 10, pady=(40,0) )
 
-label3 = ctk.CTkLabel(app, text="TEXTO LBL", fg_color="transparent")
-label3.grid(row=1, column =1, padx=20,  pady=20)
+button5= customtkinter.CTkButton(app, text="Press Me", command=call_back_btn)
+button5.grid( row=1, column=1,padx= 10, pady=(40,0) )
 
-label4 = ctk.CTkLabel(app, text="TEXTO LBL", fg_color="transparent")
-label4.grid(row=1, column =2, padx=20,  pady=20)
-
-label5 = ctk.CTkLabel(app, text="TEXTO LBL", fg_color="transparent")
-label5.grid(row=2, column =0, padx=20,  pady=20)
-
-label6 = ctk.CTkLabel(app, text="TEXTO LBL", fg_color="transparent")
-label6.grid(row=2, column =1, padx=20,  pady=20)
+button6= customtkinter.CTkButton(app, text="Press Me", command=call_back_btn)
+button6.grid( row=1, column=2,padx= 10, pady=(40,0) )
 
 app.mainloop()
